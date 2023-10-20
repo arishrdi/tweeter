5200685ebd7bc16206a9688ae0ec777202bcad62
unknown <arishardisantoso@gmail.com>
2023-10-20 16:06:39 +0700

    update database

--- src/env.mjs
+++ src/env.mjs
@@ -7,21 +7,21 @@ export const env = createEnv({
    * isn't built with invalid env vars.
    */
   server: {
-    DATABASE_URL: z
-      .string()
-      .url()
-      .refine(
-        (str) => !str.includes("YOUR_MYSQL_URL_HERE"),
-        "You forgot to change the default URL"
-      ),
+    // DATABASE_URL: z
+    //   .string()
+    //   .url()
+    //   .refine(
+    //     (str) => !str.includes("YOUR_MYSQL_URL_HERE"),
+    //     "You forgot to change the default URL"
+    //   ),
     POSTGRES_PRISMA_URL: z.string().url().refine(
       (str) => !str.includes("POSTGRES_PRISMA_URL"),
       "You forgot to change the default URL"
     ),
-    POSTGRES_URL_NON_POOLING: z.string().url().refine(
-      (str) => !str.includes("POSTGRES_URL_NON_POOLING"),
-      "You forgot to change the default URL"
-    ),
+    // POSTGRES_URL_NON_POOLING: z.string().url().refine(
+    //   (str) => !str.includes("POSTGRES_URL_NON_POOLING"),
+    //   "You forgot to change the default URL"
+    // ),
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
